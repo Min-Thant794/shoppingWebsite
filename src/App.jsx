db7124bb5ff4cfe60b4c26cab34b8c1271dee2de
Min-Thant794 <minthanthtun29@gmail.com>
2025-08-01 20:10:02 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Login'
-import Home from './home'
+import Home from './Home'
 import Deals from './Deals'
 import Product from './Product'
 import New_Arrival from './New_Arrival'
